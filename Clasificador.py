7119e718757f3acf66f6f3c8ff59228fbe63da38
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2022-07-17 15:50:39 -0300

    .

--- Clasificador.py
+++ Clasificador.py
@@ -19,7 +19,6 @@ jtplot.style()
 from Herramientas import UtilsPlot
 utilidades = UtilsPlot()
 
-
 #----------Acá nos fijamos si el procesador tiene CUDA, o sino usamos el CPU----------
 dtype = torch.float32
 if torch.cuda.is_available():
@@ -27,6 +26,66 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
 #----------Acá nos fijamos si el procesador tiene CUDA, o sino usamos el CPU----------
+
+
+def compute_acc(loader, model, eval_mode=False):
+    num_correct = 0
+    num_total = 0
+    if eval_mode: model.eval()
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=dtype)
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, pred = scores.max(1)
+            assert pred.shape == y.shape, 'Label shape and prediction shape does not match'
+            num_correct += (pred==y).sum()
+            num_total += pred.size(0)           
+            
+        return float(num_correct)/num_total
+
+def plot_loss(losses):  
+    fig = plt.figure()
+    f1 = fig.add_subplot()
+    f1.set_ylabel("Cost")
+    f1.set_xlabel("Epoch")
+    f1.set_title("Cost vs Epoch")
+    f1.plot(losses)
+    plt.show()
+
+def train(model, optimizer, epochs, loader_train):
+    model = model.to(device=device)
+    losses = []
+    
+    num_batches = len(loader_train)
+    
+    for epoch in range(epochs):
+        accum_loss = 0.
+        for i, (x, y) in enumerate(loader_train):
+            #poner modelo en modo de entrenamiento
+            model.train()
+
+            #mover a GPU
+            x = x.to(device=device, dtype=dtype)
+            y = y.to(device=device, dtype=torch.long)
+            
+            #calcular scores
+            scores = model(x)
+            cost = F.cross_entropy(input=scores, target=y)
+
+            #calcular gradients
+            optimizer.zero_grad()
+            cost.backward()
+            
+            #actualizar parametros
+            optimizer.step()
+            
+            #guardar pérdida
+            accum_loss += cost.item()
+        losses.append(accum_loss / num_batches)
+            
+        print(f'Epoch: {epoch}, loss: {cost.item()}, val accuracy: {compute_acc(loader_val, model, True)}\n')
+    plot_loss(losses)
 
 
 
@@ -56,102 +115,77 @@ loader_test = DataLoader(mnist_test, batch_size=BATCH_SIZE)
 x_test = loader_test.dataset.data
 y_test = loader_test.dataset.targets
 
-# Acá graficamos los primeros 25 datos que tenemos en el tensor x_test.
-#utilidades.PlotInGrid(5,5,x_test[0:25])
-
-
 n_inputs = len(torch.flatten(x_test[0]))        # Agarramos la matriz de pixeles de la primer imagen, la convertimos en una lista, y calculamos el largo.
-hidden1 = 1000
-hidden2 = 1000
+hidden = 512
 output = 10
 
 modelo = nn.Sequential(
     nn.Flatten(),
-    nn.Linear(in_features=n_inputs, out_features=hidden1),
+    nn.Linear(in_features=n_inputs, out_features=hidden),
     nn.ReLU(),
-    #nn.Linear(in_features=hidden1, out_features=hidden2),
-    #nn.ReLU(),
-    nn.Linear(in_features=hidden2, out_features=output),
+    nn.Linear(in_features=hidden, out_features=hidden),
+    nn.ReLU(),
+    nn.Linear(in_features=hidden, out_features=hidden),
+    nn.ReLU(),
+    nn.Linear(in_features=hidden, out_features=output),
 )
 
 
-
-def compute_acc(loader, model, eval_mode=False):
-    num_correct = 0
-    num_total = 0
-    if eval_mode: model.eval()
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device, dtype=dtype)
-            y = y.to(device=device, dtype=torch.long)
-            scores = model(x)
-            _, pred = scores.max(1)
-            assert pred.shape == y.shape, 'Label shape and prediction shape does not match'
-            num_correct += (pred==y).sum()
-            num_total += pred.size(0)           
-            
-        return float(num_correct)/num_total
-
-
-def plot_loss(losses):  
-    fig = plt.figure()
-    f1 = fig.add_subplot()
-    f1.set_ylabel("Cost")
-    f1.set_xlabel("Epoch")
-    f1.set_title("Cost vs Epoch")
-    f1.plot(losses)
-    plt.show()
-    
-def train(model, optimizer, epochs=100):
-    model = model.to(device=device)
-    losses = []
-    
-    num_batches = len(loader_train)
-    for epoch in range(epochs):
-        accum_loss = 0.
-        for i, (x, y) in enumerate(loader_train):
-            #poner modelo en modo de entrenamiento
-            model.train()
-            
-            #mover a GPU
-            x = x.to(device=device, dtype=dtype)
-            y = y.to(device=device, dtype=torch.long)
-            
-            #calcular scores
-            scores = model(x)
-            cost = F.cross_entropy(input=scores, target=y)
-
-            #calcular gradients
-            optimizer.zero_grad()
-            cost.backward()
-            
-            #actualizar parametros
-            optimizer.step()
-            
-            #guardar pérdida
-            accum_loss += cost.item()
-        losses.append(accum_loss / num_batches)
-            
-        print(f'Epoch: {epoch}, loss: {cost.item()}, val accuracy: {compute_acc(loader_val, model, True)} ')
-        print()
-    plot_loss(losses)
-
-'''
-#entrenar el modelo
-learning_rate = 1e-2
-epochs = 1
+# Entrenando el modelo
+learning_rate = 0.3#1e-2
+epochs = 5
 optimizer = torch.optim.SGD(modelo.parameters(), lr=learning_rate)
-train(modelo, optimizer, epochs)
-'''
+train(modelo, optimizer, epochs, loader_train)
 
+
+
+
+
+# Evaluamos en el set de test.
+modelo.eval()
+with torch.no_grad():
+    aciertos = 0
+    total = 0
+    for i, (x, y) in enumerate(loader_test):
+        scores = modelo(x)
+        _, pred = scores.max(1)
+        for j in range(len(pred)):
+            if pred[j]==y[j]:
+                aciertos+=1
+            total+=1
+    print(f"Aciertos:{aciertos}\nTotal:{total}\nPorcentaje de aciertos:{aciertos/total}")
+
+
+
+# Acá probamos con algunos números escritos por mi, para ver que tan bien hace la predicción.
 import torchvision
-img_prueba = torchvision.io.read_image("asd/numero3.png")
-print(img_prueba)
+acertados = []
+for i in range(10):
+    img_prueba = torchvision.io.read_image(f"escritosPorMi/numero{i}.png")
+    new_img = []
 
-modelo.eval()              # turn the model to evaluate mode
-with torch.no_grad():     # does not calculate gradient
-    class_index = modelo(img_prueba)
-print(class_index)
+    for f in range(len(img_prueba[0])):
+        new_img.append([])
+        for c in range(len(img_prueba[0][0])):
+            new_img[f].append(float(img_prueba[0][f][c]))
+    new_img = [[new_img]]
+    new_img = torch.tensor(new_img)/255
+
+    modelo.eval()
+    with torch.no_grad():
+        #print(f"----Predicción para el número {i}:")
+        scores = modelo(new_img)
+        _, prediction = scores.max(1)
+        #print(prediction)
+        if prediction.item() == i:
+            acertados.append(i)
+print(f"Los números acertados son: {acertados}")
+
+
+
+
+
+
 
 
 
@@ -163,28 +197,7 @@ img = mpimg.imread("numero3.png")
 R, G, B = img[:,:,0], img[:,:,1], img[:,:,2]
 imgGray = (0.2989 * R + 0.5870 * G + 0.1140 * B)*255            # Fórmula mágica.
 imgGray = torch.flatten(torch.tensor(imgGray, dtype=int))       # Lo hacemos tensor.
-
-
 modelo.eval()              # turn the model to evaluate mode
 with torch.no_grad():     # does not calculate gradient
     class_index = modelo(imgGray)#.argmax()   #gets the prediction for the image's class
 print(class_index)'''
-
-
-
-
-
-
-""" Esto es para ver si tenemos CUDA para procesar en nuestra computadora.
-print(torch.cuda.is_available())"""
-
-#utilidades.PlotInGrid(1,1,[x_test[1]])
-#print(y_test[1])
-
-#print(y_test.shape)
-#print(x_test.shape)
-#print(x_test[0])
-#print(type(x_test[0]))
-#plot_number(x_test[0])
-
-
